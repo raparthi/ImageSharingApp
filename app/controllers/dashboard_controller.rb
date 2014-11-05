2e155f9013c829902034d7ea844d22640df7dedc
Balakishore Raparthi <kishore@eggheadcreative.com>
2014-11-05 16:41:09 +0530

    minor functionality implemented

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def show
     @albums = current_user.albums
-    @others_albums = Album.where.not(user_id: current_user.id).last(5);
+    @others_albums = Album.where.not(user_id: current_user.id).last(25);
   end
 end
