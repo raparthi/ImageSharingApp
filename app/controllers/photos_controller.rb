ce96790efd54fc1ba7619f547970f7023f4afa58
Balakishore Raparthi <kishore@eggheadcreative.com>
2014-11-05 10:18:54 +0530

    Created new photo to album

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,10 +5,19 @@ class PhotosController < ApplicationController
   end
 
   def create
-
+    @photo = @album.photos.build(photo_params)
+    if @photo.save
+      redirect_to dashboard_path, notice: 'photo created'
+    else
+      render :new
+    end
   end
   private
   def get_album
-    @album = Album.friendly.find(params[:id])
+    @album = Album.friendly.find(params[:album_id])
+  end
+
+  def photo_params
+    params.require(:photo).permit(:image, :tag_line)
   end
 end
